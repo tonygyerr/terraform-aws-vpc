fab5b164390ebb032bf4ee5ba409ab79173e80f1
Tony Gyepi-Garbrah <a.gyepigarbrah@gmail.com>
2021-12-15 18:48:17 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,28 +1,35 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+  value = aws_vpc.this.id
+}
+
+output "eip_lb_ngw_id" {
+  value = aws_eip.this.*.id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.this.id
 }
 
 output "public_subnets" {
-  value = [module.vpc.public_subnets]
+  value = aws_subnet.public.*.id
 }
 
 output "private_subnets" {
-  value = [module.vpc.private_subnets]
+  value = aws_subnet.private.*.id
 }
 
 output "private_db_subnets" {
-  value = [module.vpc.private_db_subnets]
+  value = aws_subnet.database.*.id
 }
 
 output "security_group_id" {
-  value = [module.vpc.security_group_id]
+  value = aws_security_group.app.id
 }
- 
+
 output "route_table_pub_id" {
-  value = module.vpc.route_table_pub_id
+  value = aws_route_table.public.*.id
 }
 
 output "route_table_prv_id" {
-  value = module.vpc.route_table_prv_id
+  value = aws_route_table.private.*.id
 }
